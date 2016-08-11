5b4be54d5083355e3894a09a7dd7adc9f40bdbf5
Elliott Jenkins <elliott.jenkins@methods.co.uk>
2016-08-11 11:55:13 +0100

    configurable logging level

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 logger = logging.getLogger(__name__)
 
 LOGGING_FORMAT = "%(asctime)s|%(levelname)s: sdx-mock-receipt: %(message)s"
-LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
 
 RECEIPT_USER = os.getenv("RECEIPT_USER", "")
 RECEIPT_PASS = os.getenv("RECEIPT_PASS", "")
